c7034c0d40f900b52c80b8f53790412bb596d509
BpAmos <bpamos21@gmail.com>
2020-06-22 15:58:51 -0500

    update and create new tests (WIP)

--- tests/test_read_dict.py
+++ tests/test_read_dict.py
@@ -5,16 +5,15 @@ from src.directional_survey import *
 class TestReadDict(unittest.TestCase):
 
     def test_read_dict(self):
-        path = Path().resolve().parent
-        json_path = path / 'data/wellbore_survey_v3.json'
+        current_dir = Path.cwd()
+        path = current_dir.parent / 'data'
+        json_path = path / 'wellbore_survey_v3.json'
 
         with open(json_path) as json_file:
             data = json.load(json_file)
         json_file.close()
 
         survey_obj = Survey(data)
-        #print(survey_obj.directional_survey_points.wellId)
-        #print(survey_obj.directional_survey_points.surface_longitude)
 
         self.assertEqual(len([survey_obj.directional_survey_points.wellId]), 1, 'incorrect')
         self.assertEqual(len(survey_obj.directional_survey_points.md), 110, 'incorrect')
