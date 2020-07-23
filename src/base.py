8b76b742a15340db8c11414f69c6c222e94656ed
BpAmos <bpamos21@gmail.com>
2020-07-23 16:09:06 -0600

    removed unnecessary funcs

--- src/base.py
+++ src/base.py
@@ -1,35 +1,7 @@
 from .imports import *
 
-
+# TODO: reduce to only what you will really need
 def setify(o): return o if isinstance(o, set) else set(L(o))
-
-
-def _rm_self(sig):
-    sigd = dict(sig.parameters)
-    sigd.pop('self')
-    return sig.replace(parameters=sigd.values())
-
-
-class FixSigMeta(type):
-    "A metaclass that fixes the signature on classes that override __new__"
-
-    def __new__(cls, name, bases, dict):
-        res = super().__new__(cls, name, bases, dict)
-        if res.__init__ is not object.__init__: res.__signature__ = _rm_self(inspect.signature(res.__init__))
-        return res
-
-
-class NewChkMeta(FixSigMeta):
-    "Metaclass to avoid recreating object passed to constructor"
-
-    def __call__(cls, x=None, *args, **kwargs):
-        if not args and not kwargs and x is not None and isinstance(x, cls):
-            x._newchk = 1
-            return x
-
-        res = super().__call__(*((x,) + args), **kwargs)
-        res._newchk = 0
-        return res
 
 
 def _is_array(x): return hasattr(x, '__array__') or hasattr(x, 'iloc')
@@ -60,19 +32,57 @@ class CollBase:
     def __iter__(self): return self.items.__iter__()
 
 
-class L(CollBase, metaclass=NewChkMeta):
+class L(CollBase):
     "Behaves like a list of `items` but can also index with list of indices or masks"
     _default = 'items'
 
-    def __init__(self, items=None, *rest, use_list=False, match=None):
+    def __init__(self, items=None, *rest, use_list=False):
         if rest: items = (items,) + rest
         if items is None: items = []
         if (use_list is not None) or not _is_array(items):
             items = list(items) if use_list else _listify(items)
-        if match is not None:
-            if is_coll(match): match = len(match)
-            if len(items) == 1:
-                items = items * match
-            else:
-                assert len(items) == match, 'Match length mismatch'
         super().__init__(items)
+
+# def _rm_self(sig):
+#     sigd = dict(sig.parameters)
+#     sigd.pop('self')
+#     return sig.replace(parameters=sigd.values())
+#
+#
+# class FixSigMeta(type):
+#     "A metaclass that fixes the signature on classes that override __new__"
+#
+#     def __new__(cls, name, bases, dict):
+#         res = super().__new__(cls, name, bases, dict)
+#         if res.__init__ is not object.__init__: res.__signature__ = _rm_self(inspect.signature(res.__init__))
+#         return res
+#
+#
+# class NewChkMeta(FixSigMeta):
+#     "Metaclass to avoid recreating object passed to constructor"
+#
+#     def __call__(cls, x=None, *args, **kwargs):
+#         if not args and not kwargs and x is not None and isinstance(x, cls):
+#             x._newchk = 1
+#             return x
+#
+#         res = super().__call__(*((x,) + args), **kwargs)
+#         res._newchk = 0
+#         return res
+
+# class L(CollBase, metaclass=NewChkMeta):
+#     "Behaves like a list of `items` but can also index with list of indices or masks"
+#     _default = 'items'
+#
+#     def __init__(self, items=None, *rest, use_list=False, match=None):
+#         if rest: items = (items,) + rest
+#         if items is None: items = []
+#         if (use_list is not None) or not _is_array(items):
+#             items = list(items) if use_list else _listify(items)
+#         if match is not None:
+#             if is_coll(match): match = len(match)
+#             if len(items) == 1:
+#                 items = items * match
+#             else:
+#                 assert len(items) == match, 'Match length mismatch'
+#         super().__init__(items)
