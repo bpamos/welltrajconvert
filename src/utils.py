1d14961ca0d8b7acf2f3ffe84ce3597789175757
BpAmos <bpamos21@gmail.com>
2020-07-23 14:52:06 -0600

    added to_type funcs

--- src/utils.py
+++ src/utils.py
@@ -1,17 +1,30 @@
+"`.utils` contains essential util functions to format and split data"
 from src.imports import *
 
 
-def ifnone(a:Any,b:Any)->Any:
+def ifnone(a: Any, b: Any) -> Any:
     "`a` if `a` is not None, otherwise `b`."
     return b if a is None else a
 
-def is_dict(object):
+
+def to_type(a: Any, data_type):
+    """
+    If item is None, return None, else, convert to an data_type specified
+    (ie. np.array, str, int, float, ect..)
+
+    :parameter: a (Any or None)
+
+    :returns: None or data_type(a)
+    """
+    return None if a is None else data_type(a)
+
+
+def is_dict(obj):
     """
     Helper function that checks if a given parameter is a dict or not
     """
 
-    if isinstance(object, dict):
+    if isinstance(obj, dict):
         return True
     else:
         return False
-
