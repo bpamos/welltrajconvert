392e020a72965c1b3a452601cf700ef5d995572b
BpAmos <bpamos21@gmail.com>
2020-07-30 12:52:20 -0600

    updated with helper funcs

--- src/utils.py
+++ src/utils.py
@@ -82,3 +82,36 @@ def crs_transformer(crs_out: str, crs_in: str, x: float, y: float):
 #     return res
 
 
+# def from_multiple_wells_to_df(df: DataFrame, wellId_name: Optional[str] = None, md_name: Optional[str] = None,
+#                               inc_name: Optional[str] = None, azim_name: Optional[str] = None,
+#                               surface_latitude_name: Optional[str] = None,
+#                               surface_longitude_name: Optional[str] = None,
+#                               surface_x_name: Optional[str] = None,
+#                               surface_y_name: Optional[str] = None):
+#     # group by wellId, ensures this will work with single well or mulitple.
+#     grouped = df.groupby(wellId_name)
+#
+#     # initialize empty dict and list
+#     appended_df = pd.DataFrame()
+#     # loop through groups converting them to the proper dict format
+#     for name, group in grouped:
+#
+#         group.reset_index(inplace=True, drop=True)
+#
+#         if surface_latitude_name is not None and surface_longitude_name is not None:
+#             well_obj = WellboreTrajectory.from_df(group, wellId_name=wellId_name, md_name=md_name,
+#                                                   inc_name=inc_name, azim_name=azim_name,
+#                                                   surface_latitude_name=surface_latitude_name,
+#                                                   surface_longitude_name=surface_longitude_name)
+#         if surface_x_name is not None and surface_y_name is not None:
+#             well_obj = WellboreTrajectory.from_df(group, wellId_name=wellId_name, md_name=md_name,
+#                                                   inc_name=inc_name, azim_name=azim_name,
+#                                                   surface_x_name=surface_x_name,
+#                                                   surface_y_name=surface_y_name)
+#
+#         well_obj.calculate_survey_points()
+#         well_obj.deviation_survey_obj
+#         appended_df = appended_df.append(well_obj.get_survey_df())
+#
+#     res = appended_df
+#     return res
