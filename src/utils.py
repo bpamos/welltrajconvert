ed9f8d639e2274a9f40c7967ce977938368e2ac2
BpAmos <bpamos21@gmail.com>
2020-07-30 12:24:09 -0600

    external helper function dealing with multi well csv

--- src/utils.py
+++ src/utils.py
@@ -39,3 +39,46 @@ def crs_transformer(crs_out: str, crs_in: str, x: float, y: float):
     latitude, longitude = transformer.transform(x, y)
 
     return latitude, longitude
+
+
+## external helper functions for using the library:
+
+# def from_multiple_wells(df: DataFrame, wellId_name: Optional[str] = None, md_name: Optional[str] = None,
+#                         inc_name: Optional[str] = None, azim_name: Optional[str] = None,
+#                         surface_latitude_name: Optional[str] = None,
+#                         surface_longitude_name: Optional[str] = None,
+#                         surface_x_name: Optional[str] = None,
+#                         surface_y_name: Optional[str] = None):
+#     # group by wellId, ensures this will work with single well or mulitple.
+#     grouped = df.groupby(wellId_name)
+#
+#     # initialize empty dict and list
+#     d = {}
+#     dlist = []
+#     # loop through groups converting them to the proper dict format
+#     for name, group in grouped:
+#
+#         group.reset_index(inplace=True, drop=True)
+#
+#         if surface_latitude_name is not None and surface_longitude_name is not None:
+#             well_obj = WellboreTrajectory.from_df(group, wellId_name=wellId_name, md_name=md_name,
+#                                                   inc_name=inc_name, azim_name=azim_name,
+#                                                   surface_latitude_name=surface_latitude_name,
+#                                                   surface_longitude_name=surface_longitude_name)
+#         if surface_x_name is not None and surface_y_name is not None:
+#             well_obj = WellboreTrajectory.from_df(group, wellId_name=wellId_name, md_name=md_name,
+#                                                   inc_name=inc_name, azim_name=azim_name,
+#                                                   surface_x_name=surface_x_name,
+#                                                   surface_y_name=surface_y_name)
+#
+#         well_obj.calculate_survey_points()
+#         dataclass_obj = well_obj.deviation_survey_obj
+#
+#         # TODO: convert to dict???
+#         d = dataclass_obj
+#         dlist.append(d)
+#
+#     res = {'data': dlist}
+#     return res
+
+
