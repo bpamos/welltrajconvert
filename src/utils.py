b6aa5e3fc1027f2ce017a103f4221ca90c48b904
BpAmos <bpamos21@gmail.com>
2020-08-14 13:20:09 -0500

    moved get_files funcs into utils

--- src/utils.py
+++ src/utils.py
@@ -1,5 +1,36 @@
 "`.utils` contains essential util functions to format and split data"
-from src.imports import *
+from .base import *
+
+
+def _get_files(p, fs, extensions=None):
+    p = Path(p)
+    res = [p / f for f in fs if not f.startswith('.')
+           and ((not extensions) or f'.{f.split(".")[-1].lower()}' in extensions)]
+    return res
+
+
+def get_files(path, extensions=None, recurse=True, folders=None, followlinks=True):
+    """
+    Get all the files in `path` with optional `extensions`,
+    optionally with `recurse`, only in `folders`, if specified.
+    """
+    path = Path(path)
+    folders = L(folders)
+    extensions = make_set(extensions)
+    extensions = {e.lower() for e in extensions}
+    if recurse:
+        res = []
+        for i, (p, d, f) in enumerate(os.walk(path, followlinks=followlinks)):  # returns (dirpath, dirnames, filenames)
+            if len(folders) != 0 and i == 0:
+                d[:] = [o for o in d if o in folders]
+            else:
+                d[:] = [o for o in d if not o.startswith('.')]
+            if len(folders) != 0 and i == 0 and '.' not in folders: continue
+            res += _get_files(p, f, extensions)
+    else:
+        f = [o.name for o in os.scandir(path) if o.is_file()]
+        res = _get_files(path, f, extensions)
+    return L(res)
 
 
 def if_none(a: Any, b: Any) -> Any:
@@ -19,16 +50,6 @@ def to_type(a: Any, data_type):
     return None if a is None else data_type(a)
 
 
-def is_dict(obj):
-    """
-    Helper function that checks if a given parameter is a dict or not
-    """
-
-    if isinstance(obj, dict):
-        return True
-    else:
-        return False
-
 def crs_transformer(crs_out: str, crs_in: str, x: float, y: float):
     """
     takes a two crs and transforms x and y coordinates to latitude and longitude.
@@ -41,77 +62,10 @@ def crs_transformer(crs_out: str, crs_in: str, x: float, y: float):
     return latitude, longitude
 
 
-## external helper functions for using the library:
-
-# def from_multiple_wells(df: DataFrame, wellId_name: Optional[str] = None, md_name: Optional[str] = None,
-#                         inc_name: Optional[str] = None, azim_name: Optional[str] = None,
-#                         surface_latitude_name: Optional[str] = None,
-#                         surface_longitude_name: Optional[str] = None,
-#                         surface_x_name: Optional[str] = None,
-#                         surface_y_name: Optional[str] = None):
-#     # group by wellId, ensures this will work with single well or mulitple.
-#     grouped = df.groupby(wellId_name)
-#
-#     # initialize empty dict and list
-#     d = {}
-#     dlist = []
-#     # loop through groups converting them to the proper dict format
-#     for name, group in grouped:
-#
-#         group.reset_index(inplace=True, drop=True)
-#
-#         if surface_latitude_name is not None and surface_longitude_name is not None:
-#             well_obj = WellboreTrajectory.from_df(group, wellId_name=wellId_name, md_name=md_name,
-#                                                   inc_name=inc_name, azim_name=azim_name,
-#                                                   surface_latitude_name=surface_latitude_name,
-#                                                   surface_longitude_name=surface_longitude_name)
-#         if surface_x_name is not None and surface_y_name is not None:
-#             well_obj = WellboreTrajectory.from_df(group, wellId_name=wellId_name, md_name=md_name,
-#                                                   inc_name=inc_name, azim_name=azim_name,
-#                                                   surface_x_name=surface_x_name,
-#                                                   surface_y_name=surface_y_name)
-#
-#         well_obj.calculate_survey_points()
-#         dataclass_obj = well_obj.deviation_survey_obj
-#
-#         # TODO: convert to dict???
-#         d = dataclass_obj
-#         dlist.append(d)
-#
-#     res = {'data': dlist}
-#     return res
-
-
-# def from_multiple_wells_to_df(df: DataFrame, wellId_name: Optional[str] = None, md_name: Optional[str] = None,
-#                               inc_name: Optional[str] = None, azim_name: Optional[str] = None,
-#                               surface_latitude_name: Optional[str] = None,
-#                               surface_longitude_name: Optional[str] = None,
-#                               surface_x_name: Optional[str] = None,
-#                               surface_y_name: Optional[str] = None):
-#     # group by wellId, ensures this will work with single well or mulitple.
-#     grouped = df.groupby(wellId_name)
-#
-#     # initialize empty dict and list
-#     appended_df = pd.DataFrame()
-#     # loop through groups converting them to the proper dict format
-#     for name, group in grouped:
-#
-#         group.reset_index(inplace=True, drop=True)
-#
-#         if surface_latitude_name is not None and surface_longitude_name is not None:
-#             well_obj = WellboreTrajectory.from_df(group, wellId_name=wellId_name, md_name=md_name,
-#                                                   inc_name=inc_name, azim_name=azim_name,
-#                                                   surface_latitude_name=surface_latitude_name,
-#                                                   surface_longitude_name=surface_longitude_name)
-#         if surface_x_name is not None and surface_y_name is not None:
-#             well_obj = WellboreTrajectory.from_df(group, wellId_name=wellId_name, md_name=md_name,
-#                                                   inc_name=inc_name, azim_name=azim_name,
-#                                                   surface_x_name=surface_x_name,
-#                                                   surface_y_name=surface_y_name)
-#
-#         well_obj.calculate_survey_points()
-#         well_obj.deviation_survey_obj
-#         appended_df = appended_df.append(well_obj.get_survey_df())
-#
-#     res = appended_df
-#     return res
+def list_of_dicts_to_df(dict_list):
+    """takes a list of dicts and converts to a appended df"""
+    appended_df = pd.DataFrame()
+    for i in dict_list:
+        df_well_obj = pd.DataFrame(i)
+        appended_df = appended_df.append(df_well_obj)
+    return appended_df
